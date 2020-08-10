cb5c05ce3d6e1994506e8ca5d50df2e9e740fc9c
Unknown <edanuramail@gmail.com>
2020-08-10 09:33:19 -0400

    Camera Fixed

---
+++ Assets/Scripts/GabeCam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GabeCam : MonoBehaviour
+{
+	public Transform player;
+
+	public float distanceFromPlayer;
+
+	public float lerpSpeed = .2f;
+	
+	private void Awake()
+	{
+	}
+
+	private void Start()
+	{
+//		distanceFromPlayer = transform.position.z;
+	}
+
+	void FixedUpdate ()
+	{
+		/*
+		transform.position = new Vector3(Mathf.Lerp(transform.position.x, player.position.x, lerpSpeed/2), transform.position.y, 
+			Mathf.Lerp(transform.position.z, player.position.z + distanceFromPlayer, lerpSpeed) );
+			*/
+		
+		transform.position = new Vector3(Mathf.Lerp(transform.position.x, player.position.x, lerpSpeed), transform.position.y, 
+			transform.position.z );
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+    
+    private KeyCode up = KeyCode.UpArrow;
+    private KeyCode down = KeyCode.DownArrow;
+    private KeyCode right = KeyCode.RightArrow;
+    private KeyCode left = KeyCode.LeftArrow;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(down))
+        {
+            transform.position += Vector3.back*Time.deltaTime * speed;
+        }
+        
+        if (Input.GetKey(up))
+        {
+            transform.position += Vector3.forward*Time.deltaTime * speed;
+        }
+        
+        if (Input.GetKey(left))
+        {
+            transform.position += Vector3.left*Time.deltaTime * speed;
+        }
+        
+        if (Input.GetKey(right))
+        {
+            transform.position += Vector3.right*Time.deltaTime * speed;
+        }
+    }
+}
